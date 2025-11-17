8423843216768b4d3fa6c1bd86fd0cd59fd80c24
Stas Mykhailenko <stas.m@2muchcoffee.com>
2025-11-17 10:34:45 +0200

    build (app): add pgvector

---
+++ migrations/1763133912742-add-pgvector.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddPgvector1763133912742 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS vector`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP EXTENSION IF EXISTS vector`);
+  }
+}
